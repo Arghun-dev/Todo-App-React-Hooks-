c2c19f55c8dc91447c086c087cdfb3c52308af9e
Arghun-dev <arghun.developer@gmail.com>
2020-03-08 14:18:49 +0330

    Created TodoForm and some Styling of TodoApp

--- todoapp/src/TodoApp.js
+++ todoapp/src/TodoApp.js
@@ -2,21 +2,25 @@ import React from 'react';
 import {withStyles} from '@material-ui/styles';
 import Paper from '@material-ui/core/Paper';
 import TodoForm from './TodoForm';
+import Container from '@material-ui/core/Container';
 
 const styles = {
     root: {
         marginLeft: '10%',
         marginRight: '10%',
-        marginTop: '5%'
+        marginTop: '5%',
+        padding: '5%'
     }
 }
 
 function TodoApp(props) {
     const {classes} = props
     return (
-        <Paper className={classes.root}>
-            <TodoForm />
-        </Paper>
+        <Container maxWidth='md'>
+            <Paper className={classes.root}>
+                <TodoForm />
+            </Paper>
+        </Container>
     )
 }
 
--- todoapp/src/TodoForm.js
+++ todoapp/src/TodoForm.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import {withStyles} from '@material-ui/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import TextField from '@material-ui/core/TextField';
 
-function TodoForm() {
+const styles = {
+    addIcon: {
+        float: 'right'
+    },
+
+    textField: {
+        width: '50%'
+    }
+}
+
+function TodoForm(props) {
+    const {classes} = props
     return(
-        <h1>TodoApp</h1>
+        <form noValidate autoComplete="off">
+            <TextField 
+                id="standard-basic" 
+                label="New Todo" 
+                className={classes.textField} 
+            />
+            <Fab color="primary" aria-label="add" className={classes.addIcon}>
+                <AddIcon />
+            </Fab>
+        </form>
     )
 }
 
-export default TodoForm;
+export default withStyles(styles)(TodoForm);
